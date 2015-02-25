c861e2b40184034059529e9fc61688cbb71f0b27
Prelang Builder <builder@prelang.com>
2015-02-25 22:57:36 +0000

    Defined validation for attribute 'website' on model 'Contact'

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,5 @@ class Contact < ActiveRecord::Base
   belongs_to :email
   validates_formatting_of :email, using: :email
   validates_formatting_of :picture, using: :url
+  validates_formatting_of :website, using: :url
 end
