1fddd16b6771eaeadd6140c311e7e6f03d025863
Prelang Builder <builder@prelang.com>
2015-02-25 22:57:49 +0000

    Made model 'Contact' votable by adding acts_as_votable

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,4 +3,5 @@ class Contact < ActiveRecord::Base
   validates_formatting_of :email, using: :email
   validates_formatting_of :picture, using: :url
   validates_formatting_of :website, using: :url
+  acts_as_votable 
 end
